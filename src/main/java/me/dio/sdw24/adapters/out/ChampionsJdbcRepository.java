542346778916a648cef244eafff40917523d5130
GuilhermeHP8 <guilherme.pires.moraes@gmail.com>
2024-04-01 22:34:55 -0300

    Problens with API solveds

--- src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
+++ src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
@@ -35,8 +35,8 @@ public class ChampionsJdbcRepository implements ChampionsRepository {
     @Override
     public Optional<Champion> findById(Long id) {
         String sql = "SELECT * FROM CHAMPIONS WHERE ID = ?";
-        Champion champion = jdbcTemplate.queryForObject(sql, rowMapper, id);
-        return Optional.ofNullable(champion);
+        List<Champion> champion = jdbcTemplate.query(sql, rowMapper, id);
+        return champion.stream().findFirst();
     }
 
 }
